20bc508cda019595b06fc3b7f2b7f957e9e54579
Sven Aarts <sven@designisdead.com>
2017-03-18 11:43:28 +0100

    fit video, cleanup, smooth scroll

---
+++ js/fitex.js
@@ -0,0 +1,13 @@
+$(function() {
+    var $root = $('html, body');
+    $('.navbar-nav a').click(function (e) {
+        e.preventDefault();
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top - 100
+        }, 750, function () {
+            window.location.hash = href;
+        });
+        return false;
+    });
+});
